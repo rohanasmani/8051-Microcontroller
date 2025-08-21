5f0046ef5c74825691dfb11eee155acf13de8cf5
Rohan Asmani <rohanasmani@gmail.com>
2025-08-21 18:15:18 +0530

    Create Basic Data transfer code.asm

---
+++ Basic Data transfer code.asm
@@ -0,0 +1,8 @@
+org 0000h
+ mov a, #10h     ; Load immediate value 10h into accumulator A
+ mov r2, #10h    ; Load immediate value 10h into register R2
+ mov r0, a       ; Copy contents of accumulator A into register R0
+ mov r1, #30h    ; Load immediate value 30h into register R1
+ mov r1, r0      ; Copy contents of register R0 into register R1 (overwrites previous 30h)
+ mov 10h, #0FFh  ; Store immediate value FFh into internal RAM location 10h
+end
